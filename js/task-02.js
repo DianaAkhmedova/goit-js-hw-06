ab8e3f64cff3487d577b57f18cdbc599352cc4b1
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2022-11-03 22:19:39 +0200

    done task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listIngredientsEL = document.querySelector('#ingredients');
+
+const list = ingredients.map(ingredient => {
+  const itemsEl = document.createElement('li');
+  itemsEl.textContent = ingredient;
+  itemsEl.classList.add('item');
+
+  return itemsEl;
+});
+
+listIngredientsEL.append(...list);
