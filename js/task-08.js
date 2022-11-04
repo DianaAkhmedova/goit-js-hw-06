592cd515565271635b04a755457d0c73ad540d75
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2022-11-04 16:16:42 +0200

    done task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const email =
+    event.currentTarget.elements.email.value.trim() !== ''
+      ? event.currentTarget.elements.email.value
+      : alert('Всі поля повинні бути заповнені!');
+
+  const password =
+    event.currentTarget.elements.password.value.trim() !== ''
+      ? event.currentTarget.elements.password.value
+      : alert('Всі поля повинні бути заповнені!');
+
+  const formData = {
+    email,
+    password,
+  };
+
+  form.reset();
+
+  console.log(formData);
+};
+
+form.addEventListener('submit', onFormSubmit);
