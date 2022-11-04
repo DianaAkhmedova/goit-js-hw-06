217fc9318bd1ca5dd265b957c2b8751908566aab
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2022-11-04 16:57:07 +0200

    done task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const button = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+const changeColor = event => {
+  const colorHex = (body.style.backgroundColor = getRandomHexColor());
+  colorValue.textContent = `${colorHex}`;
+};
+
+button.addEventListener('click', changeColor);
