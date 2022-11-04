4998c4fb1303ea6e6233e8711380ca6162b03b09
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2022-11-04 21:40:08 +0200

    add 8, 10 task

--- js/task-08.js
+++ js/task-08.js
@@ -3,15 +3,25 @@ const form = document.querySelector('.login-form');
 const onFormSubmit = event => {
   event.preventDefault();
 
-  const email =
-    event.currentTarget.elements.email.value.trim() !== ''
-      ? event.currentTarget.elements.email.value
-      : alert('Всі поля повинні бути заповнені!');
+  const elements = event.currentTarget.elements;
 
-  const password =
-    event.currentTarget.elements.password.value.trim() !== ''
-      ? event.currentTarget.elements.password.value
-      : alert('Всі поля повинні бути заповнені!');
+  for (const element of elements) {
+    if (element.name) {
+      if (element.value.trim() === '') alert('Всі поля повинні бути заповнені!');
+    }
+  }
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  // const email =
+  //   event.currentTarget.elements.email.value.trim() !== ''
+  //     ? event.currentTarget.elements.email.value
+  //     : alert('Всі поля повинні бути заповнені!');
+
+  // const password =
+  //   event.currentTarget.elements.password.value.trim() !== ''
+  //     ? event.currentTarget.elements.password.value
+  //     : alert('Всі поля повинні бути заповнені!');
 
   const formData = {
     email,
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const box = document.querySelector('#boxes');
+// console.log(box);
+
+let amount = 0;
+const getAmount = () => {
+  amount = event.currentTarget.value;
+  console.log(amount);
+};
+input.addEventListener('input', getAmount);
+
+const newBoxes = document.createElement('div');
+
+function createBoxes(amount) {
+  newBoxes.style.width = '30px';
+  newBoxes.style.height = '30px';
+  newBoxes.style.backgroundColor = getRandomHexColor();
+  return box.append(newBoxes);
+}
+
+buttonCreate.addEventListener('click', () => {
+  createBoxes(amount);
+});
+
+const destroyBoxes = () => {
+  newBoxes.remove();
+};
+
+buttonDestroy.addEventListener('click', destroyBoxes);
