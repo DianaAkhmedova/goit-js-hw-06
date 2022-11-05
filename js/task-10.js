a783cc9efc2b5931da7984f5c49f56f5aa02ab38
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2022-11-05 23:55:21 +0200

    done task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,34 +2,41 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const input = document.querySelector('input');
-const buttonCreate = document.querySelector('button[data-create]');
-const buttonDestroy = document.querySelector('button[data-destroy]');
-const box = document.querySelector('#boxes');
-// console.log(box);
+const refs = {
+  input: document.querySelector('input'),
+  buttonCreate: document.querySelector('button[data-create]'),
+  buttonDestroy: document.querySelector('button[data-destroy]'),
+  box: document.querySelector('#boxes'),
+};
 
 let amount = 0;
-const getAmount = () => {
-  amount = event.currentTarget.value;
-  console.log(amount);
+const getAmount = event => {
+  amount = event.target.value;
 };
-input.addEventListener('input', getAmount);
-
-const newBoxes = document.createElement('div');
 
 function createBoxes(amount) {
-  newBoxes.style.width = '30px';
-  newBoxes.style.height = '30px';
-  newBoxes.style.backgroundColor = getRandomHexColor();
-  return box.append(newBoxes);
+  let boxSize = 20;
+
+  for (let i = 1; i <= amount; i += 1) {
+    const newBoxes = document.createElement('div');
+
+    boxSize += 10;
+
+    newBoxes.style.width = `${boxSize}px`;
+    newBoxes.style.height = `${boxSize}px`;
+    newBoxes.style.backgroundColor = getRandomHexColor();
+    refs.box.append(newBoxes);
+  }
 }
 
-buttonCreate.addEventListener('click', () => {
+const handleClickButtonCreate = () => {
   createBoxes(amount);
-});
-
-const destroyBoxes = () => {
-  newBoxes.remove();
 };
 
-buttonDestroy.addEventListener('click', destroyBoxes);
+const destroyBoxes = () => {
+  refs.box.innerHTML = '';
+};
+
+refs.input.addEventListener('change', getAmount);
+refs.buttonCreate.addEventListener('click', handleClickButtonCreate);
+refs.buttonDestroy.addEventListener('click', destroyBoxes);
